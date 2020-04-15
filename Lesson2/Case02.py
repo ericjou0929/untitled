349fbd0cabf884f5907f6d8953b7e83e35fbb042
ericjou0929 <a0963327198@gamil.com>
2020-04-15 20:37:39 +0800

    123

---
+++ Lesson2/Case02.py
@@ -0,0 +1,13 @@
+# -*- coding:UTF-8 -*-
+
+# 轉型態
+
+a = "100"
+b = "90.5"
+
+print(a+b)
+print(int(a)+float(b))
+
+
+score = 95
+print ("成績=" + str(score))
---
+++ Lesson2/Case04.py
@@ -0,0 +1,13 @@
+# -*- coding:UTF-8 -*-
+
+score = 75
+print("score = " , score , sep = "=")
+print("score = " , score, sep =",")
+print("score = " , score, sep = "&" )
+
+print("a")
+print("b")
+
+print("a", end="")
+print("b", end="")
+print("c", end="\n")
