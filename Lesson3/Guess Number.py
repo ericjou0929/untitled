d5b25135c1e095b9d770747f44024c378f751990
ericjou0929 <a0963327198@gamil.com>
2020-04-22 21:03:26 +0800

    123

--- Lesson3/Guess Number.py
+++ Lesson3/Guess Number.py
@@ -1,9 +1,28 @@
+# import random as r
+# ans = r.randint(1,99)
+# min = 0
+# max = 100
+# while True:
+#     Guess = int(input('請輸入數字 %d ~ %d :' % (min , max)))
+#     if Guess == ans:
+#         print("恭喜答對了")
+#         break;
+#     elif Guess < ans:
+#         min = Guess
+#     else:
+#         max = Guess
+
 import random as r
 ans = r.randint(1,99)
 min = 0
 max = 100
 while True:
     Guess = int(input('請輸入數字 %d ~ %d :' % (min , max)))
+    # 先驗證數字是否在範圍內
+    if Guess <= min or Guess >= max:
+        print("數字範圍錯誤,重新輸入 : ")
+        continue
+    # 進行遊戲比對
     if Guess == ans:
         print("恭喜答對了")
         break;
@@ -12,4 +31,4 @@ while True:
     else:
         max = Guess
 
-        
+
